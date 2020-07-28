af1cdd8127aec2ca4a1e30c13b4c99420138b25b
Jacob Kirby <65231998+Kirby5253@users.noreply.github.com>
2020-07-28 16:41:29 -0500

    changed directorView to function

--- client/src/components/director-view/director-view.jsx
+++ client/src/components/director-view/director-view.jsx
@@ -4,54 +4,51 @@ import PropTypes from 'prop-types';
 import './director-view.scss';
 import { connect } from 'react-redux';
 
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 
-class DirectorView extends React.Component {
-	constructor() {
-		super();
+/**
+ * allows users to type in the details that they want to change of their account
+ * @function DirectorView
+ * @requires react
+ * @requires react-bootstrap/Button
+ * @requires './director-view.scss'
+ * @requires react-router-dom
+ * @requires react-redux
+ */
+function DirectorView() {
+  const { director } = this.props;
 
-		this.state = {};
-	}
+  return (
+    <div className="director-view">
+      <div className="director-name">
+        <span className="label">Director Name: </span>
+        <span className="value">{director.Name}</span>
+      </div>
+      <br />
+      <div className="director-bio">
+        <span className="label">Biography: </span>
+        <span className="value">{director.Bio}</span>
+      </div>
+      <br />
+      <div className="director-birth">
+        <span className="label">Born: </span>
+        <span className="value">{director.Birth}</span>
+      </div>
 
-	render() {
-		const { director } = this.props;
-
-		return (
-			<div className="director-view">
-				<div className="director-name">
-					<span className="label">Director Name: </span>
-					<span className="value">{director.Name}</span>
-				</div>
-        <br />
-				<div className="director-bio">
-					<span className="label">Biography: </span>
-					<span className="value">{director.Bio}</span>
-				</div>
-        <br />
-        <div className="director-birth">
-					<span className="label">Born: </span>
-					<span className="value">{director.Birth}</span>
-				</div>
-
-				<br />
-				<Link to={`/`} className="home-button">
-					<Button variant="link">
-						Home
-					</Button>
-				</Link>
-			</div>
-		);
-	}
+      <br />
+      <Link to={`/`} className="home-button">
+        <Button variant="link">Home</Button>
+      </Link>
+    </div>
+  );
 }
 
 DirectorView.propTypes = {
-	director: PropTypes.shape({
-		Name: PropTypes.string.isRequired,
-		Bio: PropTypes.string.isRequired,
-		Birth: PropTypes.string.isRequired
-	}).isRequired
+  director: PropTypes.shape({
+    Name: PropTypes.string.isRequired,
+    Bio: PropTypes.string.isRequired,
+    Birth: PropTypes.string.isRequired,
+  }).isRequired,
 };
-
-
 
 export default connect(null, {})(DirectorView);
