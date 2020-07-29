673297728ee0b3f4a4dc5589315fa6c4e9687f0e
Jacob Kirby <65231998+Kirby5253@users.noreply.github.com>
2020-07-28 20:22:44 -0500

    jsdoc genre and login view

--- client/src/components/genre-view/genre-view.jsx
+++ client/src/components/genre-view/genre-view.jsx
@@ -3,40 +3,79 @@ import Button from 'react-bootstrap/Button';
 import PropTypes from 'prop-types';
 import './genre-view.scss';
 import { connect } from 'react-redux';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
 
-import { Link } from "react-router-dom";
-
+/**
+ * allows users to see the details that they want to view about movie genres.
+ * only accessible from movie view currently
+ * @class GenreView
+ * @requires react
+ * @requires axios
+ * @requires react-bootstrap/Button
+ * @requires './director-view.scss'
+ * @requires react-router-dom
+ * @requires react-redux
+ */
 class GenreView extends React.Component {
-	render() {
-    const { genre } = this.props;
+  constructor() {
+    super();
 
-    
-		return (
-			<div className="genre-view">
-				<div className="genre-name">
-					<span className="label">Genre Name: </span>
-					<span className="value">{genre.Name}</span>
-				</div> <br />
-				<div className="movie-description">
-					<span className="label">Description: </span>
-					<span className="value">{genre.Description}</span>
-				</div>
-				<br />
-				<Link to={`/`}>
-					<Button className="genre-button" variant="link">
-						Home
-					</Button>
-				</Link>
-			</div>
-		);
-	}
+    this.state = { genreInfo: '' };
+  }
+
+  /**
+	 * handles getting the info for the related genre
+	 * @param {string} name genre name
+	 * @returns {object} Genre name and description
+	 */
+  getGenreInfo(name) {
+    const token = localStorage.getItem('token');
+    axios
+      .get(`https://myflixdb5253.herokuapp.com/genres/${name}`, {
+        headers: { Authorization: `Bearer ${token}` },
+      })
+      .then((response) => {
+        this.setState({ genreInfo: response.data });
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+  }
+
+  componentDidMount() {
+    this.getGenreInfo(this.props.name);
+  }
+  render() {
+    const { genreInfo } = this.state;
+
+    return (
+      <div className="genre-view">
+        <div className="genre-name">
+          <span className="label">Genre Name: </span>
+          <span className="value">{genreInfo.Name}</span>
+        </div>{' '}
+        <br />
+        <div className="movie-description">
+          <span className="label">Description: </span>
+          <span className="value">{genreInfo.Description}</span>
+        </div>
+        <br />
+        <Link to={`/`}>
+          <Button className="genre-button" variant="link">
+            Home
+          </Button>
+        </Link>
+      </div>
+    );
+  }
 }
 
 GenreView.propTypes = {
-	genre: PropTypes.shape({
-		Name: PropTypes.string.isRequired,
-		Description: PropTypes.string.isRequired,
-	})
+  genre: PropTypes.shape({
+    Name: PropTypes.string.isRequired,
+    Description: PropTypes.string.isRequired,
+  }),
 };
 
 export default connect(null, {})(GenreView);
--- client/src/components/login-view/login-view.jsx
+++ client/src/components/login-view/login-view.jsx
@@ -4,97 +4,114 @@ import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
-
 import { connect } from 'react-redux';
 
+/**
+ * allows users to type in the id of their favorite movie to remove it from their account list
+ * @function LoginView
+ * @requires react
+ * @requires react-bootstrap/Form
+ * @requires './change-favorites-view.scss'
+ * @requires axios
+ * @requires react-router-dom
+ * @requires react-redux
+ */
 function LoginView(props) {
-	const [ username, setUsername ] = useState('');
-	const [ password, setPassword ] = useState('');
-	const storedUser = localStorage.getItem('user');
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const storedUser = localStorage.getItem('user');
 
-	// Has ability to login with random credentials for existing user, no functionality for new users yet
-	const handleSubmit = (e) => {
-		e.preventDefault();
-		/* Send a request to the server for authentication */
-		axios
-			.post('https://myflixdb5253.herokuapp.com/login', {
-				Username: username,
-				Password: password
-			})
-			.then((response) => {
-				const data = response.data;
-				props.onLoggedIn(data);
-			})
-			.catch((e) => {
-				console.log('no such user');
-			});
-	};
+  /**
+	 * handles the post to db to login user to app
+	 * @function handleSubmit
+	 * @param {*} e
+	 */
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    /* Send a request to the server for authentication */
+    axios
+      .post('https://myflixdb5253.herokuapp.com/login', {
+        Username: username,
+        Password: password,
+      })
+      .then((response) => {
+        const data = response.data;
+        props.onLoggedIn(data);
+      })
+      .catch((e) => {
+        console.log('no such user');
+      });
+  };
 
-	// Allows user to push enter instead of clicking login
-	const handleSubmitEnter = (e) => {
-		if (event.key === 'Enter') {
-			/* Send a request to the server for authentication */
-			axios
-				.post('https://myflixdb5253.herokuapp.com/login', {
-					Username: username,
-					Password: password
-				})
-				.then((response) => {
-					const data = response.data;
-					props.onLoggedIn(data);
-				})
-				.catch((e) => {
-					console.log('no such user');
-				});
-		}
-	};
+  /**
+	 * allows user to push enter instead of clicking login
+	 * @function handleSubmitEnter
+	 */
+  const handleSubmitEnter = () => {
+    if (event.key === 'Enter') {
+      /* Send a request to the server for authentication */
+      axios
+        .post('https://myflixdb5253.herokuapp.com/login', {
+          Username: username,
+          Password: password,
+        })
+        .then((response) => {
+          const data = response.data;
+          props.onLoggedIn(data);
+        })
+        .catch((e) => {
+          console.log('no such user');
+        });
+    }
+  };
 
-	if (storedUser) return null;
+  // auto login for returning users
+  if (storedUser) return null;
 
-	return (
-		<div className="login-form">
-			<Form className>
-				<Form.Label>
-					<h3 className="login-title">Login to MyFlix</h3>
-				</Form.Label>
-				<Form.Group controlId="formBasicUsername">
-					<Form.Label>Username:</Form.Label>
-					<Form.Control
-						type="text"
-						placeholder="Username"
-						value={username}
-						onChange={(e) => setUsername(e.target.value)}
-					/>
-				</Form.Group>
+  return (
+    <div className="login-form">
+      <Form className>
+        <Form.Label>
+          <h3 className="login-title">Login to MyFlix</h3>
+        </Form.Label>
+        <Form.Group controlId="formBasicUsername">
+          <Form.Label>Username:</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </Form.Group>
 
-				<Form.Group controlId="formBasicPassword">
-					<Form.Label>Password</Form.Label>
-					<Form.Control
-						onKeyPress={handleSubmitEnter}
-						type="password"
-						placeholder="Password"
-						value={password}
-						onChange={(e) => setPassword(e.target.value)}
-					/>
-				</Form.Group>
-				<br />
-				<Button variant="dark" className="login-button" type="button" onClick={handleSubmit}>
-					Login
-				</Button>
-				<Link to={`/register`}>
-					<Button variant="link" className="login-button" type="button">
-						New to MyFlix? Click here to create an account
-					</Button>
-				</Link>
-				<br />
-			</Form>
-		</div>
-	);
+        <Form.Group controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            onKeyPress={handleSubmitEnter}
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </Form.Group>
+        <br />
+        <Button variant="dark" className="login-button" type="button" onClick={handleSubmit}>
+          Login
+        </Button>
+        <Link to={`/register`}>
+          <Button variant="link" className="login-button" type="button">
+            New to MyFlix? Click here to create an account
+          </Button>
+        </Link>
+        <br />
+      </Form>
+    </div>
+  );
 }
 
 LoginView.propTypes = {
-	setUsername: PropTypes.string,
-	setPassword: PropTypes.string
+  setUsername: PropTypes.string,
+  setPassword: PropTypes.string,
 };
 
 export default connect(null, {})(LoginView);
